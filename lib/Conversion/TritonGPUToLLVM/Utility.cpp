flagos-ai/FlagTree

commit 7cc7db0c3ee5a7aaef7066ac856848c24202e6ac

[LinearLayouts] Faster pext algorithm (#5621)
We also skip the LinearLayout test for HIP as it's currently failing.

Regarding the use of `getWarpSize` and `getNumWarpsPerCTA`, which are
not correct for LinearLayouts with broadcasting as noted in
https://github.com/triton-lang/triton/pull/5617, we found almost all the
uses are in AMD land. Changing these into calling the functions that act
on the module is tricky, as the module is not currently accessible at
the caller site in most of them. As such, we leave this refactor up to
AMD folks.

lib/Conversion/TritonGPUToLLVM/Utility.cpp
@@ -12,11 +12,18 @@
 // from https://gist.github.com/pps83/3210a2f980fd02bb2ba2e5a1fc4a2ef0
 #include <intrin.h>
 
+static int __builtin_clz(unsigned x) {
+  unsigned long r;
+  _BitScanReverse(&r, x);
+  return static_cast<int>(r);
+}
+
 static int __builtin_ctz(unsigned x) {
   unsigned long r;
   _BitScanForward(&r, x);
   return static_cast<int>(r);
 }
+
 #endif
 
 namespace mlir {
@@ -601,18 +608,22 @@ Value pext_i32(RewriterBase &rewriter, Location loc, Value a, uint32_t mask) {
   if (mask == 0xFFFFFFFF)
     return a;
 
-  // We implement a blocked algorithm to avoid generating too many instructions
+  // Implements the blocked algorithm from
+  // https://forums.developer.nvidia.com/t/pdep-and-pext-functionality-for-cuda/270973
+  uint32_t mskConst = mask;
+  uint32_t extcnt = 0;
   Value result = i32_val(0);
-  int resultPos = 0;
-  while (mask) {
-    int start = __builtin_ctz(mask);
-    int width = __builtin_ctz(~(mask >> start));
-    Value shifted = lshr(a, i32_val(start));
-    Value widthMask = i32_val(((1u << width) - 1));
-    Value blockVal = and_(shifted, widthMask);
-    result = or_(result, shl(blockVal, i32_val(resultPos)));
-    resultPos += width;
-    mask &= ~(((1u << width) - 1) << start);
+  while (mskConst) {
+    uint32_t oldmsk = mskConst;
+    uint32_t bitgrplsb = mskConst & (-mskConst);
+    mskConst &= bitgrplsb + mskConst;
+    uint32_t bitgrp = mskConst ^ oldmsk;
+    uint32_t lsbpos = 31 - __builtin_clz(bitgrplsb);
+    // like popcount for a number 0..01..1..0 but portable
+    uint32_t grplen = __builtin_ctz(~(bitgrp >> lsbpos));
+    uint32_t shift = lsbpos - extcnt;
+    extcnt += grplen;
+    result = or_(result, lshr(and_(i32_val(bitgrp), a), i32_val(shift)));
   }
   return result;
 }

lib/Dialect/TritonGPU/Transforms/AccelerateMatmul.cpp
@@ -447,7 +447,8 @@ public:
         mmaEnc.getInstrShape()[versionMajor == 3
                                    ? 0
                                    : mmaEnc.getInstrShape().size() - 2];
-    auto warpSize = getWarpSize(newAEncoding);
+    auto mod = scaledDotOp->getParentOfType<ModuleOp>();
+    int warpSize = triton::gpu::TritonGPUDialect::getThreadsPerWarp(mod);
     assert(instrShapeM <= warpSize);
     // Necessary choice to leave all the scales of the tile in that given warp
     auto threadsPerWarp =

python/test/unit/language/test_core.py
@@ -2793,6 +2793,8 @@ def test_reduce_layouts(M, N, src_layout, axis, epilogue_kind, dtype_str, add_ov
         pytest.skip("Skipping test because it runs out of shared memory")
     if reduce_op == "sum" and dtype_str == "float16" and M * N > 1024:
         pytest.skip("Skipping sum reduction on float16 due to accuracy issues")
+    if is_hip() and isinstance(src_layout, LinearLayout):
+        pytest.skip("FIXME: LinearLayout not supported on HIP")
 
     if isinstance(src_layout, MmaLayout) and src_layout.version == 3:
         src_layout[2] = 16 if dtype_str == "float16" else 8

commit 4e45edc02ba031cb89e29bbe917285a8bad4149d

[BACKEND] Fix `__builtin_clz` implementation on Windows (#5774)
This PR fixes a typo in the Windows implementation of `__builtin_clz`
that was introduced in #5621.

According to [this in-code
comment](https://github.com/triton-lang/triton/blob/1fd03f869f953c762a6f5196642ac21f0d53725a/lib/Conversion/TritonGPUToLLVM/Utility.cpp#L12)
these Windows implementations should have been copied from [this gist
snippet](https://gist.github.com/pps83/3210a2f980fd02bb2ba2e5a1fc4a2ef0).
In the snippet however the `clz` implementation additionally [XORs the
result of
`_BitScanReverse`](https://gist.github.com/pps83/3210a2f980fd02bb2ba2e5a1fc4a2ef0#file-ctz_clz-cpp-L51-L53)
in order to convert the result from the <i>most significant bit</i>
produced by `_BitScanReverse` to the expected <i>number of leading
zeros</i>. I believe the implementation was copied to the triton without
the finalizing XOR by accident.

<b>What is affected by this error?</b>
This implementation of CLZ is used in
[`pext_i32`](https://github.com/intel/intel-xpu-backend-for-triton/blob/4a9967137548f8fe9b1a93383e4fd12646352231/lib/Conversion/TritonGPUToLLVM/Utility.cpp#L635)
that is used in
[`delinearize`](https://github.com/intel/intel-xpu-backend-for-triton/blob/4a9967137548f8fe9b1a93383e4fd12646352231/lib/Conversion/TritonGPUToLLVM/Utility.cpp#L662)
that is used by
[`ReduceOpToLLVM`](https://github.com/intel/intel-xpu-backend-for-triton/blob/4a9967137548f8fe9b1a93383e4fd12646352231/lib/Conversion/TritonGPUToLLVM/ReduceOpToLLVM.cpp#L243-L247)
pattern. This bug caused `tt.reduce()` ops to be incorrectly lowered on
Windows in cases, where shared memory is needed to store temporary
reduced results.

Signed-off-by: dchigarev <dmitry.chigarev@intel.com>

lib/Conversion/TritonGPUToLLVM/Utility.cpp
@@ -15,7 +15,7 @@
 static int __builtin_clz(unsigned x) {
   unsigned long r;
   _BitScanReverse(&r, x);
-  return static_cast<int>(r);
+  return static_cast<int>(r ^ 31);
 }
 
 static int __builtin_ctz(unsigned x) {