256576185c0c78115243bd92dd9b051bcc404bf4
toudefu <2696810816@qq.com>
2025-11-27 16:31:38 +0800

    decouple: use __has_include

--- third_party/ascend/backend/flagtree_backend_specialization/include/flagtree_spec.h
+++ third_party/ascend/backend/flagtree_backend_specialization/include/flagtree_spec.h
@@ -1,7 +1,6 @@
 #ifndef ASCEND_FLAGTREE_SPEC_H_
 #define ASCEND_FLAGTREE_SPEC_H_
 
-#include "triton/Dialect/Triton/IR/ascend_Dialect.h"
 #include "triton/Dialect/Triton/IR/ascend_Ops.h"
 #include "triton/Dialect/Triton/IR/ascend_Traits.h"
 
--- third_party/ascend/backend/flagtree_backend_specialization/include/triton/Dialect/Triton/IR/ascend_Dialect.h
+++
@@ -1,8 +0,0 @@
-// TODO: When upgrading to Triton 3.4.0, remove this file and use the upstream Triton file.
-#ifndef ASCEND_TRITON_DIALECT_TRITON_IR_DIALECT_H_
-#define ASCEND_TRITON_DIALECT_TRITON_IR_DIALECT_H_
-
-#define FLAGTREE_SPEC_Dialect_Triton_IR_OpInterfaces_head
-#define FLAGTREE_SPEC_Dialect_Triton_IR_OpInterfaces_inc
-
-#endif // ASCEND_TRITON_DIALECT_TRITON_IR_DIALECT_H_
