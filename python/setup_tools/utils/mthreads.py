b533e37350b734b7b56899009ac133931a3fdf92
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2025-12-19 16:41:53 +0800

    Update resource URLs and hashes for aarch64 architecture

--- python/setup_tools/utils/mthreads.py
+++ python/setup_tools/utils/mthreads.py
@@ -7,7 +7,7 @@ def get_resources_url(resource_name):
         return {
             "llvm":
             "https://baai-cp-web.ks3-cn-beijing.ksyuncs.com/trans/mthreads-llvm19-glibc2.35-glibcxx3.4.30-aarch64_v0.4.0.tar.gz",
-            'plugin':
+            "plugin":
             "https://baai-cp-web.ks3-cn-beijing.ksyuncs.com/trans/mthreadsTritonPlugin-cpython3.10-glibc2.35-glibcxx3.4.30-cxxabi1.3.13-ubuntu-aarch64_v0.4.0.tar.gz"
         }[resource_name]
     elif arch == 'x86_64':
@@ -22,6 +22,6 @@ def get_resources_url(resource_name):
 def get_resources_hash(resource_name):
     arch = platform.machine()
     if arch == 'aarch64':
-        return {'plugin': "2f5f04a2"}[resource_name]
+        return {'plugin': "1a31d294"}[resource_name]
     elif arch == 'x86_64':
         return {"plugin": "2f5f04a2"}[resource_name]
