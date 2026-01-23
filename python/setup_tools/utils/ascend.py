a8b76c611433ca239cab33a002d532d704d2abf0
baai <sunwenjia04@163.com>
2026-01-23 11:18:19 +0800

    Changes to be committed: 	modified:   python/setup_tools/utils/ascend.py

--- python/setup_tools/utils/ascend.py
+++ python/setup_tools/utils/ascend.py
@@ -4,16 +4,17 @@ from .tools import flagtree_configs, DownloadManager, Module
 downloader = DownloadManager()
 
 submodules = (Module(name="ascendnpu-ir", url="https://gitcode.com/Ascend/AscendNPU-IR.git",
-                     commit_id="04045a06ec7c9592b17de659307d5debe7be590a", dst_path=os.path.join(flagtree_configs.flagtree_submodule_dir, "ascendnpu-ir")), )
+                     commit_id="04045a06ec7c9592b17de659307d5debe7be590a",
+                     dst_path=os.path.join(flagtree_configs.flagtree_submodule_dir, "ascendnpu-ir")), )
 
 
 def precompile_hook_flir(*args, **kargs):
     default_backends = kargs["default_backends"]
-    default_backends_list=list(default_backends)
+    default_backends_list = list(default_backends)
     if 'amd' in default_backends:
-            default_backends_list.remove('amd')
+        default_backends_list.remove('amd')
     default_backends_list.append('flir')
-    default_backends=tuple(default_backends_list)
+    default_backends = tuple(default_backends_list)
     get_submodule()
 
 
