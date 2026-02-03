a19ada454c56eca745e58f07788254cee530f8df
SabYic <sunwenjia04@163.com>
2026-02-03 17:39:53 +0800

    modified:   python/setup_tools/setup_helper.py

--- python/setup_tools/setup_helper.py
+++ python/setup_tools/setup_helper.py
@@ -7,17 +7,11 @@ import hashlib
 import sysconfig
 from distutils.sysconfig import get_python_lib
 from . import utils
+from .utils.tools import flagtree_configs as configs
 
-extend_backends = []
-default_backends = ["nvidia", "amd"]
-plugin_backends = ["cambricon", "ascend", "aipu", "tsingmicro"]
-ext_sourcedir = "triton/_C/"
-flagtree_backend = os.getenv("FLAGTREE_BACKEND", "").lower()
-flagtree_plugin = os.getenv("FLAGTREE_PLUGIN", "").lower()
-offline_build = os.getenv("FLAGTREE_PLUGIN", "OFF")
-device_mapping = {"xpu": "xpu", "mthreads": "musa", "ascend": "ascend", "sunrise": "sunrise"}
-activated_module = utils.activate(flagtree_backend)
 downloader = utils.tools.DownloadManager()
+configs = configs
+flagtree_backend = configs.flagtree_backend
 
 set_llvm_env = lambda path: set_env(
     {
@@ -37,7 +31,7 @@ def yield_backend_dirs():
 
 def install_extension(*args, **kargs):
     try:
-        activated_module.install_extension(*args, **kargs)
+        configs.activated_module.install_extension(*args, **kargs)
     except Exception:
         pass
 
@@ -50,7 +44,7 @@ def get_backend_cmake_args(*args, **kargs):
     handle_plugin_backend(editable)
     try:
         # cmake_args = configs.activated_module.get_backend_cmake_args(*args, **kargs)
-        cmake_args = activated_module.get_backend_cmake_args(*args, **kargs)
+        cmake_args = configs.activated_module.get_backend_cmake_args(*args, **kargs)
     except Exception:
         cmake_args = []
     if editable:
@@ -59,13 +53,13 @@ def get_backend_cmake_args(*args, **kargs):
 
 
 def get_device_name():
-    return device_mapping[flagtree_backend]
+    return configs.device_alias_map[flagtree_backend]
 
 
 def get_extra_packages():
     packages = []
     try:
-        packages = activated_module.get_extra_install_packages()
+        packages = configs.activated_module.get_extra_install_packages()
     except Exception:
         packages = []
     return packages
@@ -74,7 +68,7 @@ def get_extra_packages():
 def get_package_data_tools():
     package_data = ["compile.h", "compile.c"]
     try:
-        package_data += activated_module.get_package_data_tools()
+        package_data += configs.activated_module.get_package_data_tools()
     except Exception:
         package_data
     return package_data
@@ -100,8 +94,8 @@ def download_flagtree_third_party(name, condition, required=False, hock=None):
             submodule = utils.flagtree_submodules[name]
             downloader.download(module=submodule, required=required)
             if callable(hock):
-                hock(third_party_base_dir=utils.flagtree_submodule_dir, backend=submodule,
-                     default_backends=default_backends)
+                configs.default_backends=hock(third_party_base_dir=utils.flagtree_configs.flagtree_submodule_dir, backend=submodule,
+                     default_backends=configs.default_backends)
 
         else:
             print(f"\033[1;33m[Note] Skip downloading {name} since USE_{name.upper()} is set to OFF\033[0m")
@@ -109,7 +103,7 @@ def download_flagtree_third_party(name, condition, required=False, hock=None):
 
 def post_install():
     try:
-        activated_module.post_install()
+        configs.activated_module.post_install()
     except Exception:
         pass
 
@@ -270,16 +264,16 @@ class CommonUtils:
         if 'backends' in package or 'profiler' in package:
             return True
         try:
-            return activated_module.skip_package_dir(package)
+            return configs.activated_module.skip_package_dir(package)
         except Exception:
             return False
 
     @staticmethod
     def get_package_dir(packages):
         package_dict = {}
-        if flagtree_backend and flagtree_backend not in plugin_backends:
+        if configs.flagtree_backend and configs.flagtree_backend not in configs.plugin_backends:
             connection = []
-            backend_triton_path = f"./third_party/{flagtree_backend}/python/"
+            backend_triton_path = f"./third_party/{configs.flagtree_backend}/python/"
             for package in packages:
                 if CommonUtils.skip_package_dir(package):
                     continue
@@ -287,7 +281,7 @@ class CommonUtils:
                 connection.append(pair)
             package_dict.update(connection)
         try:
-            package_dict.update(activated_module.get_package_dir())
+            package_dict.update(configs.activated_module.get_package_dir())
         except Exception:
             pass
         return package_dict
@@ -295,20 +289,20 @@ class CommonUtils:
 
 def handle_flagtree_backend():
     global ext_sourcedir
-    if flagtree_backend:
-        print(f"\033[1;32m[INFO] FlagtreeBackend is {flagtree_backend}\033[0m")
-        extend_backends.append(flagtree_backend)
-        if "editable_wheel" in sys.argv and flagtree_backend not in plugin_backends:
-            ext_sourcedir = os.path.abspath(f"./third_party/{flagtree_backend}/python/{ext_sourcedir}") + "/"
+    if configs.flagtree_backend:
+        print(f"\033[1;32m[INFO] FlagtreeBackend is {configs.flagtree_backend}\033[0m")
+        configs.extend_backends.append(configs.flagtree_backend)
+        if "editable_wheel" in sys.argv and configs.flagtree_backend not in configs.plugin_backends:
+            ext_sourcedir = os.path.abspath(f"./third_party/{configs.flagtree_backend}/python/{ext_sourcedir}") + "/"
 
 
 def handle_plugin_backend(editable):
     plugin_mode = os.getenv("FLAGTREE_PLUGIN")
     if plugin_mode and plugin_mode.upper() not in ["0", "OFF"]:
         return
-    flagtree_backend_dir = Path.home() / ".flagtree" / flagtree_backend
-    flagtree_plugin_so = flagtree_backend + "TritonPlugin.so"
-    if flagtree_backend in ["iluvatar", "mthreads", "sunrise"]:
+    flagtree_backend_dir = Path.home() / ".flagtree" / configs.flagtree_backend
+    flagtree_plugin_so = configs.flagtree_backend + "TritonPlugin.so"
+    if configs.flagtree_backend in ["iluvatar", "mthreads", "sunrise"]:
         if editable is False:
             src_build_plugin_path = flagtree_backend_dir / flagtree_plugin_so
             dst_build_plugin_dir = Path(sysconfig.get_path("purelib")) / "triton" / "_C"
@@ -384,9 +378,9 @@ cache.store(
 )
 
 cache.store(
-    file="iluvatarTritonPlugin.so", condition=("iluvatar" == flagtree_backend) and (not flagtree_plugin), url=
+    file="iluvatarTritonPlugin.so", condition=("iluvatar" == configs.flagtree_backend) and (not configs.flagtree_plugin), url=
     "https://baai-cp-web.ks3-cn-beijing.ksyuncs.com/trans/iluvatarTritonPlugin-cpython3.10-glibc2.30-glibcxx3.4.28-cxxabi1.3.12-ubuntu-x86_64_v0.3.0.tar.gz",
-    copy_dst_path=f"third_party/{flagtree_backend}", md5_digest="015b9af8")
+    copy_dst_path=f"third_party/{configs.flagtree_backend}", md5_digest="015b9af8")
 
 # klx xpu
 cache.store(
@@ -423,7 +417,7 @@ cache.store(
 )
 
 cache.store(
-    file="mthreadsTritonPlugin.so", condition=("mthreads" == flagtree_backend) and (not flagtree_plugin), url=
+    file="mthreadsTritonPlugin.so", condition=("mthreads" == flagtree_backend) and (not configs.flagtree_plugin), url=
     "https://baai-cp-web.ks3-cn-beijing.ksyuncs.com/trans/mthreadsTritonPlugin-cpython3.10-glibc2.35-glibcxx3.4.30-cxxabi1.3.13-ubuntu-x86_64_v0.3.0.tar.gz",
     copy_dst_path=f"third_party/{flagtree_backend}", md5_digest="2a9ca0b8")
 
@@ -498,6 +492,6 @@ cache.store(
 )
 
 cache.store(
-    file="sunriseTritonPlugin.so", condition=("sunrise" == flagtree_backend) and (not flagtree_plugin), url=
+    file="sunriseTritonPlugin.so", condition=("sunrise" == flagtree_backend) and (not configs.flagtree_plugin), url=
     "https://baai-cp-web.ks3-cn-beijing.ksyuncs.com/trans/sunriseTritonPlugin-cpython3.10-glibc2.39-glibcxx3.4.33-x86_64_v0.4.0.tar.gz",
     copy_dst_path=f"third_party/{flagtree_backend}", md5_digest="1f0b7e67")
