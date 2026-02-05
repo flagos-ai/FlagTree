61e3dc6fea55018b21185b74321bbb691eb4fb24
Liwei Dai <liwei.dai@iluvatar.com>
2026-02-05 12:58:56 +0800

    [SWFW-2769] Fix fp32 dot
    [SWFW-2769] loose atol for fa bwd on QS.
    
    [SWFW-2769] skip noinline UT and fix test_load_store_same_ptr UT.
    
    [SWFW-2769] add TRITON_DISABLE_STP env to disable llvm.bi.stp.vs.pred and set TRITON_DISABLE_STP for related UT on QS.

--- python/test/unit/operators/test_matmul.py
+++ python/test/unit/operators/test_matmul.py
@@ -1,3 +1,4 @@
+import os
 import itertools
 
 import pytest
@@ -115,6 +116,8 @@ def is_hip():
 def test_op(BLOCK_M, BLOCK_N, BLOCK_K, SPLIT_K, NWARP, NSTAGE, M, N, K, AT, BT, ADTYPE, BDTYPE, INPUT_PRECISION,
             F8_FASTACCUM, ACC_DTYPE, OUTPUT_DTYPE):
     capability = torch.cuda.get_device_capability()
+    if capability[0] == 8:
+        os.environ["TRITON_DISABLE_STORE_STP"] = "1"
     if capability[0] < 7:
         pytest.skip("Only test tl.dot() on devices with sm >= 70")
     if capability[0] < 8 and (ADTYPE == "bfloat16" or BDTYPE == "bfloat16"):
