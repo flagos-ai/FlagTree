95d62fedf4d083ea738239ab1b88845f7125e504
zhengyang <zhengyang@baai.ac.cn>
2026-03-12 17:34:23 +0800

    [BUILD] Fix precompile_hock on tsingmicro

--- python/setup_tools/utils/tsingmicro.py
+++ python/setup_tools/utils/tsingmicro.py
@@ -5,6 +5,9 @@ def precompile_hock(*args, **kargs):
     default_backends = kargs["default_backends"]
     default_backends_list = list(default_backends)
     default_backends_list.append('flir')
+    kargs["default_backends"] = tuple(default_backends_list)
+    default_backends = tuple(default_backends_list)
+    return default_backends
 
 
 def get_backend_cmake_args(*args, **kargs):
