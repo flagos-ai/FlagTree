4a1ae40711fc9ac2bf36723ba3f632dde87edda9
SabYic <sunwenjia04@163.com>
2026-04-20 14:40:11 +0800

    Changes to be committed: 	new file:   third_party/tle/utils/include/analyzereturn.h 	new file:   third_party/tle/utils/lib/analyzereturn.cpp

---
+++ third_party/tle/utils/lib/analyzereturn.cpp
@@ -0,0 +1,158 @@
+#include "tle/utils/include/analyzereturn.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/LLVMIR/LLVMTypes.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "tle/dialect/include/IR/Dialect.h"
+#include "llvm/ADT/STLExtras.h"
+
+using namespace mlir;
+
+namespace mlir::triton::tle::dataanalyze {
+
+/// Returns true if this OriginSet changed.
+bool OriginSet::merge(const OriginSet &other) {
+  bool changed = false;
+  if (other.conflict && !conflict) {
+    conflict = true;
+    changed = true;
+  }
+  for (int64_t idx : other.indices) {
+    if (indices.insert(idx))
+      changed = true;
+  }
+  return changed;
+}
+
+bool OriginSet::operator!=(const OriginSet &other) const {
+  if (conflict != other.conflict)
+    return true;
+  if (indices.size() != other.indices.size())
+    return true;
+  for (int64_t idx : indices) {
+    if (!other.indices.contains(idx))
+      return true;
+  }
+  return false;
+}
+
+
+int64_t getDslArgIdx(BlockArgument blockArg,
+                     ArrayRef<int64_t> funcArgToDslArg) {
+  int64_t funcIdx = blockArg.getArgNumber();
+  if (funcIdx >= 0 && funcIdx < (int64_t)funcArgToDslArg.size())
+    return funcArgToDslArg[funcIdx];
+  return -1;
+}
+
+
+DenseMap<Value, OriginSet>
+computeDslArgOrigins(LLVM::LLVMFuncOp func,
+                     ArrayRef<int64_t> funcArgToDslArg) {
+  DenseMap<Value, OriginSet> origins;
+
+  // Initialize all block arguments with their DSL arg origin
+  for (Block &block : func.getBlocks()) {
+    for (BlockArgument arg : block.getArguments()) {
+      int64_t dslIdx = getDslArgIdx(arg, funcArgToDslArg);
+      OriginSet &os = origins[arg];
+      if (dslIdx >= 0)
+        os.indices.insert(dslIdx);
+      else
+        os.conflict = true; // block arg not mapped to any DSL arg
+    }
+  }
+
+  // Iterate until fixpoint
+  bool changed = true;
+  while (changed) {
+    changed = false;
+    for (Block &block : func.getBlocks()) {
+      for (Operation &op : block.getOperations()) {
+        // undef/poison: empty set (neutral element), leave as default
+        if (isa<LLVM::UndefOp, LLVM::PoisonOp>(op)) {
+          for (Value result : op.getResults()) {
+            origins.try_emplace(result);
+          }
+          continue;
+        }
+
+        // Compute merged origin of all operands
+        OriginSet opOrigin;
+        if (op.getNumOperands() == 0) {
+          opOrigin.conflict = true;
+        } else {
+          for (Value operand : op.getOperands()) {
+            auto it = origins.find(operand);
+            if (it != origins.end())
+              opOrigin.merge(it->second);
+          }
+        }
+
+        // Propagate to all results
+        for (Value result : op.getResults()) {
+          OriginSet &resultOrigin = origins[result];
+          if (resultOrigin.merge(opOrigin))
+            changed = true;
+        }
+      }
+    }
+  }
+
+  return origins;
+}
+
+SmallVector<int64_t>
+analyzeFuncReturnAliases(LLVM::LLVMFuncOp func,
+                         ArrayRef<int64_t> funcArgToDslArg) {
+  // Run set-based forward origin propagation
+  DenseMap<Value, OriginSet> origins =
+      computeDslArgOrigins(func, funcArgToDslArg);
+
+  // Find the return op
+  LLVM::ReturnOp retOp = nullptr;
+  func.walk([&](LLVM::ReturnOp op) { retOp = op; });
+  if (!retOp || retOp.getNumOperands() == 0)
+    return {};
+
+  Value retVal = retOp.getOperand(0);
+
+  // Query the return value's origin set
+  auto it = origins.find(retVal);
+  if (it == origins.end())
+    return {};
+
+  const OriginSet &retOrigin = it->second;
+
+  // If conflict (any non-DSL-arg origin mixed in), return is unreachable
+  if (retOrigin.conflict)
+    return {};
+
+  // If empty origin set, return is unreachable
+  if (retOrigin.indices.empty())
+    return {};
+
+  // Return DSL arg indices in insertion order (already deduplicated by SetVector)
+  return SmallVector<int64_t>(retOrigin.indices.begin(),
+                              retOrigin.indices.end());
+}
+
+
+SmallVector<int64_t>
+computeFuncArgToDslArg(const std::vector<Value> &args) {
+  SmallVector<int64_t> mapping;
+  int64_t dslArgIdx = 0;
+  for (const Value &arg : args) {
+    Type ty = arg.getType();
+    size_t numFuncArgs = 1;
+    if (auto tensorTy = dyn_cast<RankedTensorType>(ty))
+      numFuncArgs = 3 + 2 * tensorTy.getRank();
+    else if (auto memdescTy = dyn_cast<mlir::triton::gpu::MemDescType>(ty))
+      numFuncArgs = 3 + 2 * memdescTy.getShape().size();
+    for (size_t i = 0; i < numFuncArgs; ++i)
+      mapping.push_back(dslArgIdx);
+    dslArgIdx++;
+  }
+  return mapping;
+}
+
+} // namespace mlir::triton::tle::dataanalyze
