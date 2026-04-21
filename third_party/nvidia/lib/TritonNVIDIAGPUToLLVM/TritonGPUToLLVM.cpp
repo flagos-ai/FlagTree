d9e149e828f70e7ff874c204723e232dd5aa4e7c
lzllx123 <1803100521@qq.com>
2026-04-21 09:32:10 +0800

    [TLE]Fix the issue of extract_tile and insert_tile allocating separate smem

--- third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TritonGPUToLLVM.cpp
+++ third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TritonGPUToLLVM.cpp
@@ -177,9 +177,13 @@ struct ConvertTritonGPUToLLVM
       mlir::triton::tle::populateLocalPointersOpToLLVMPatterns(
           typeConverter, targetInfo, patterns, benefit);
       mlir::triton::tle::populateExtractTileOpToLLVMPatterns(typeConverter,
-                                                             patterns, benefit);
+                                                             patterns,
+                                                             targetInfo,
+                                                             benefit);
       mlir::triton::tle::populateInsertTileOpToLLVMPatterns(typeConverter,
-                                                            patterns, benefit);
+                                                            patterns,
+                                                            targetInfo,
+                                                            benefit);
       mlir::triton::tle::populateExclusiveCumsumOpToLLVMPatterns(
           typeConverter, targetInfo, patterns, benefit);
       if (failed(applyPartialConversion(mod, target, std::move(patterns)))) {
