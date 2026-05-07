7e3a104f7881e7b6a6457610bf32c9d7705382e3
git-flyer <1140818030@qq.com>
2026-05-07 21:20:12 +0800

    sfdgdg

--- third_party/nvidia/backend/driver.c
+++ third_party/nvidia/backend/driver.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-
+////
 // Raises a Python exception and returns false if code is not CUDA_SUCCESS.
 static bool gpuAssert(CUresult code, const char *file, int line) {
   if (code == CUDA_SUCCESS)
