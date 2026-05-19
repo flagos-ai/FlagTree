b080ae5b91a28b87d06726fda2dcda5c46fc1491
zhzhcookie <zhengyang@baai.ac.cn>
2026-05-19 21:04:55 +0800

    [SPEC] Fix hcu spec python

--- python/triton/compiler/__init__.py
+++ python/triton/compiler/__init__.py
@@ -1,7 +1,3 @@
-from triton.runtime.driver import spec_path
-
-spec_path(__path__)
-
 from .compiler import CompiledKernel, ASTSource, IRSource, compile, make_backend, LazyDict, get_cache_key
 from .errors import CompilationError
 
