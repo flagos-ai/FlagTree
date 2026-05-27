050493b26882fdc5b79ac628bf5a62c4d8d9a48a
QiLin Gai <qilin.gai@mthreads.com>
2026-05-27 15:17:16 +0800

    [BACKEND][MTHREADS] Support local binary and upate to commit 9878b0c3
    [MTHREADS] pick e09dcef6
    
    [MTHREADS] pick ad9cb3f3
    
    [MTHREADS] pick 083176ff
    
    [MTHREADS] pick 38e5a5a9
    
    [MTHREADS] pick 38e5a5a9
    
    [MTHREADS] pick 73b61004
    
    [MTHREADS] pick 26d844bd
    
    [MTHREADS] pick 9878b0c3
    
    [MTHREADS] Support local binary

--- third_party/mthreads/musa/lib/TritonMUSAGPUToLLVM/AllocateSharedMemory.cpp
+++ third_party/mthreads/musa/lib/TritonMUSAGPUToLLVM/AllocateSharedMemory.cpp
@@ -200,20 +200,25 @@ static unsigned getMusaScratchSizeInBytes(Operation *op,
 
   auto srcTy = cvtOp.getSrc().getType();
   auto dstTy = cvtOp.getType();
-  if (!cvtNeedsSharedMemory(srcTy, dstTy))
-    return 0;
 
   Attribute srcLayout = srcTy.getEncoding();
   Attribute dstLayout = dstTy.getEncoding();
-  if (useMusaReplicatedScratch(srcLayout, dstLayout) &&
-      !isSqmmaAccumulatorToBlockedLike(srcLayout, dstLayout))
-    return getFullLogicalScratchBytes(srcTy);
+  if (useMusaReplicatedScratch(srcLayout, dstLayout)) {
+    if (!isSqmmaAccumulatorToBlockedLike(srcLayout, dstLayout))
+      return getFullLogicalScratchBytes(srcTy);
+
+    bool separateRepScratch =
+        mlir::triton::musa_gpu::needsMusaRepDisjointGenericScratch(srcTy, dstTy,
+                                                                   targetInfo);
+    auto elems = getNumScratchElemsSwizzledCvt(srcTy, dstTy, targetInfo,
+                                               separateRepScratch);
+    return elems * getBitwidth(srcTy) / 8;
+  }
 
   if (useConservativeCarrierScratch(srcTy, dstTy))
     return getFullLogicalScratchBytes(srcTy);
 
-  if (isSqmmaAccumulatorToBlockedLike(srcLayout, dstLayout) ||
-      useMusaSqmmaBlockSwizzling(srcTy, dstTy) ||
+  if (useMusaSqmmaBlockSwizzling(srcTy, dstTy) ||
       useMusaGenericBlockSwizzling(srcTy, dstTy)) {
     bool separateRepScratch =
         mlir::triton::musa_gpu::needsMusaRepDisjointGenericScratch(srcTy, dstTy,
@@ -222,6 +227,9 @@ static unsigned getMusaScratchSizeInBytes(Operation *op,
                                                separateRepScratch);
     return elems * getBitwidth(srcTy) / 8;
   }
+
+  if (!cvtNeedsSharedMemory(srcTy, dstTy))
+    return 0;
 
   return defaultAllocationAnalysisScratchSizeFn(op);
 }
