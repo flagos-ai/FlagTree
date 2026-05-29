cb154f65c4894ea33557747282664a6ff4ffefbc
zhzhcookie <zhengyang@baai.ac.cn>
2026-05-30 03:15:44 +0800

    [Ascend] Fix build

--- python/setup_tools/utils/ascend.py
+++ python/setup_tools/utils/ascend.py
@@ -56,10 +56,10 @@ submodules = (Module(name="AscendNPU-IR", url="https://gitcode.com/Ascend/Ascend
 def precompile_hook_flir(*args, **kargs):
     default_backends = kargs["default_backends"]
     default_backends_list = list(default_backends)
-    if 'nvidia' in default_backends:
-        default_backends_list.remove('nvidia')
-    if 'amd' in default_backends:
-        default_backends_list.remove('amd')
+    #if 'nvidia' in default_backends:
+    #    default_backends_list.remove('nvidia')
+    #if 'amd' in default_backends:
+    #    default_backends_list.remove('amd')
     default_backends_list.append('flir')
     default_backends = tuple(default_backends_list)
     kargs["default_backends"] = default_backends
