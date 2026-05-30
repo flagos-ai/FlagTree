f4a26d6fda70fb82e2fb22f16d6678a9896261c9
zhzhcookie <zhengyang@baai.ac.cn>
2026-05-30 17:44:20 +0800

    [Ascend] Fix ascend build without tle

--- lib/Conversion/TritonToTritonGPU/TritonGPUConversion.cpp
+++ lib/Conversion/TritonToTritonGPU/TritonGPUConversion.cpp
@@ -7,7 +7,9 @@
 #include "mlir/Dialect/UB/IR/UBOps.h"
 #include "mlir/IR/IRMapping.h"
 #include "mlir/Support/LLVM.h"
+#ifdef __TLE__
 #include "tle/dialect/include/IR/Dialect.h" // flagtree tle raw
+#endif
 #include "triton/Dialect/Triton/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/Transforms/Utility.h"
@@ -86,14 +88,18 @@ TritonGPUConversionTarget::TritonGPUConversionTarget(
   // Some ops from SCF are illegal
   addIllegalOp<scf::ExecuteRegionOp, scf::ParallelOp, scf::ReduceOp,
                scf::ReduceReturnOp>();
+#ifdef __TLE__
   // flagtree tle raw
   addDynamicallyLegalOp<triton::gpu::LocalAllocOp, triton::gpu::LocalStoreOp,
                         triton::gpu::LocalLoadOp>(
       [&](Operation *op) { return isDynamicallyLegal(op, typeConverter); });
+#endif
   addDynamicallyLegalDialect<arith::ArithDialect, math::MathDialect,
                              triton::TritonDialect, cf::ControlFlowDialect,
                              scf::SCFDialect, ub::UBDialect,
+#ifdef __TLE__
                              LLVM::LLVMDialect // flagtree tle raw
+#endif
                              >(
       [&](Operation *op) { return isDynamicallyLegal(op, typeConverter); });
 
