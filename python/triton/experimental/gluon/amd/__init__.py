19d0894f828cc5445a33c88dd6ce79cff4e74fd5
zhzhcookie <zhengyang@baai.ac.cn>
2026-07-16 10:52:57 +0800

    [SPEC] Fix spec_path in gluon init

--- python/triton/experimental/gluon/amd/__init__.py
+++ python/triton/experimental/gluon/amd/__init__.py
@@ -1,8 +1,3 @@
-from triton.flagtree_spec import spec_path
-
-# flagtree backend path specialization
-spec_path(__path__)
-
 from . import gfx1250
 
 __all__ = ["gfx1250"]
--- python/triton/experimental/gluon/nvidia/__init__.py
+++ python/triton/experimental/gluon/nvidia/__init__.py
@@ -1,8 +1,3 @@
-from triton.flagtree_spec import spec_path
-
-# flagtree backend path specialization
-spec_path(__path__)
-
 from . import hopper
 from . import blackwell
 
