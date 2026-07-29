77c91999d6e946b574bd7c39bfc8e2f663158391
zhzhcookie <zhengyang@baai.ac.cn>
2026-07-29 13:07:04 +0800

    [SPEC] Spec ppu cpp files

--- third_party/ppu/spec_cpp/lib/Dialect/TritonGPU/Transforms/Pipeliner/AssignLatencies.cpp
+++ third_party/ppu/spec_cpp/lib/Dialect/TritonGPU/Transforms/Pipeliner/AssignLatencies.cpp
@@ -145,7 +145,7 @@ public:
         return false;
       }
     }
-    if (isa<tt::DescriptorLoadOp, tt::DescriptorGatherOp>(op))
+    if (isa<tt::DescriptorLoadOp, tt::DescriptorGatherOp, tt::AIULoadOp>(op))
       return true;
 #ifdef __TLE__
     bool hasLocalAllocUser = llvm::any_of(op->getUsers(), [&](Operation *user) {
@@ -345,7 +345,8 @@ loadOpsToIndirectionLevel(scf::ForOp forOp, bool pipelineWithoutDot,
       [&](Operation *op, Operation *finalUser, int distance) {
         if (!seen.insert(op).second || excluded.count(op))
           return;
-        if (isa<tt::LoadOp, tt::DescriptorLoadOp, tt::DescriptorGatherOp>(op)) {
+        if (isa<tt::LoadOp, tt::DescriptorLoadOp, tt::DescriptorGatherOp,
+                tt::AIULoadOp>(op)) {
           if (!AssignLoadLatencies::isPipeliningBeneficial(
                   op, finalUser, axisInfoAnalysis, filterSmall))
             return;
@@ -398,7 +399,8 @@ loadOpsToIndirectionLevel(scf::ForOp forOp, bool pipelineWithoutDot,
   // that are not directly used by dot ops.
   if (pipelineWithoutDot) {
     for (Operation &op : forOp.getBody()->without_terminator()) {
-      if (!isa<tt::LoadOp, tt::DescriptorLoadOp, tt::DescriptorGatherOp>(op))
+      if (!isa<tt::LoadOp, tt::DescriptorLoadOp, tt::DescriptorGatherOp,
+               tt::AIULoadOp>(op))
         dfs(&op, &op, 0);
     }
   }
