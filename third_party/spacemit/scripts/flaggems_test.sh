e84c3eadd07f6a56b3e99e38227020a6b9d2ff25
zuoweixia <weixia.zuo@spacemit.com>
2026-08-06 16:56:13 +0800

    remove useless files

--- third_party/spacemit/scripts/flaggems_test.sh
+++
@@ -1,7 +0,0 @@
-!/bin/bash
-
-set -e
-python3 -m pytest tests/test_v1_ops.py --ref=cpu
-python3 -m pytest tests/test_v2_ops.py --ref=cpu
-python3 -m pytest tests/test_v3_ops.py --ref=cpu
-python3 tests/model_resnet18_test.py
