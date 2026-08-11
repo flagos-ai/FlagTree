88fb6c6232f12855a683882540ff7d4a4fc55499
Stardep <1486216685@qq.com>
2026-08-11 21:35:20 +0800

    [SPEC][METAX] Unify Python backend specialization

--- python/setup_tools/utils/metax.py
+++ python/setup_tools/utils/metax.py
@@ -18,17 +18,5 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import shutil
-from pathlib import Path
-
-
 def install_extension(*args, **kargs):
-    # Copy knobs.py
-    _python_dir = Path(__file__).parent.parent.parent
-    src_triton_dir = _python_dir / "../third_party/metax/python/triton"
-    dst_triton_dir = _python_dir / "triton"
-
-    # copy runtime/driver.py
-    shutil.copy(src_triton_dir / "runtime/driver.py", dst_triton_dir / "runtime/driver.py")
-    # copy language/core.py semantic.py target_info.py
-    shutil.copytree(src_triton_dir / "language", dst_triton_dir / "language", dirs_exist_ok=True)
+    return
