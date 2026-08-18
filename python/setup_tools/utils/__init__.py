1147143d26299fc67719b4276119709c9925443c
zhzhcookie <zhengyang@baai.ac.cn>
2026-08-18 15:02:58 +0800

    [FlagPrism] Check newest version

--- python/setup_tools/utils/__init__.py
+++ python/setup_tools/utils/__init__.py
@@ -14,7 +14,7 @@ flagtree_submodules = {
                  dst_path=os.path.join(flagtree_configs.flagtree_submodule_dir, "flir")),
     "flagprism":
     tools.Module(name="FlagPrism", url="https://github.com/flagos-ai/FlagPrism.git",
-                 commit_id="9473b528c44966d1f52319faaa2423af6c2d9018",
+                 # commit_id="9473b528c44966d1f52319faaa2423af6c2d9018",
                  dst_path=os.path.join(flagtree_configs.flagtree_submodule_dir, "FlagPrism")),
 }
 
