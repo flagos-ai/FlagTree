b2cdd77f04ae43cf30858c44f2a86972fe4815d2
zhzhcookie <zhengyang@baai.ac.cn>
2026-08-26 14:17:53 +0800

    [TLE] Add supported tle primitives for every backend

--- third_party/ppu/backend/tle_supported.py
+++ third_party/ppu/backend/tle_supported.py
@@ -2,9 +2,11 @@ TLE_SUPPORTED_PRIMITIVES = [
     "load",
     "extract_tile",
     "insert_tile",
+    "distributed_barrier",
     "cumsum",
     "gpu.alloc",
     "gpu.copy",
     "gpu.local_ptr",
     "gpu.buffered_tensor.slot",
+    "gpu.pipeline",  # TODO: del
 ]
