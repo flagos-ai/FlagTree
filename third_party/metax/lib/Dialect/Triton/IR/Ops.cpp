2ef17b5b546d48591b9a27d7ac055669c9cc4ea8
Stardep <1486216685@qq.com>
2026-09-18 06:56:39 +0800

    [SPEC][METAX] Preserve LoadOp property ABI

--- third_party/metax/lib/Dialect/Triton/IR/Ops.cpp
+++ third_party/metax/lib/Dialect/Triton/IR/Ops.cpp
@@ -82,16 +82,6 @@ void LoadOp::build(OpBuilder &builder, OperationState &state, Value ptr,
                 evict, isVolatile);
 }
 
-void LoadOp::build(OpBuilder &builder, OperationState &state, Type result,
-                   Value ptr, Value mask, Value other,
-                   ArrayRef<int32_t> boundaryCheck, PaddingOptionAttr padding,
-                   CacheModifier cache, EvictionPolicy evict, bool isVolatile,
-                   ArrayRef<int64_t> contiguityInterConstGroup) {
-  LoadOp::build(builder, state, result, ptr, mask, other, boundaryCheck,
-                padding, cache, evict, isVolatile, contiguityInterConstGroup,
-                "");
-}
-
 // load(ptr, splat(1), ...)        -> load(ptr, ...)
 // load(ptr, splat(0), other, ...) -> other
 struct CanonicalizeMaskedLoadPattern : public OpRewritePattern<LoadOp> {
