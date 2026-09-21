0d1ec2482a115707796d0c638959325bbdcc0ba4
Stardep <1486216685@qq.com>
2026-09-22 03:54:28 +0800

    [SPEC][METAX] Preserve load attrs in MACA pipeline

--- third_party/metax/plugin/lib/TritonMETAXGPUTransforms/PipelineMACABasic.cpp
+++ third_party/metax/plugin/lib/TritonMETAXGPUTransforms/PipelineMACABasic.cpp
@@ -781,7 +781,8 @@ void LoopPipeliner::emitPrologue() {
               lookupOrDefault(loadOp.getPtr(), stage), newMask,
               lookupOrDefault(loadOp.getOther(), stage),
               loadOp.getBoundaryCheckAttr(), loadOp.getPaddingAttr(),
-              loadOp.getCache(), loadOp.getEvict(), loadOp.getIsVolatile());
+              loadOp.getCache(), loadOp.getEvict(), loadOp.getIsVolatile(),
+              loadOp.getContiguityInterConstGroup());
           loadStage[loadOp].push_back(newOp->getResult(0));
           if (stage > 1) {
             auto newLoadOp = llvm::dyn_cast<triton::LoadOp>(newOp);
@@ -809,9 +810,10 @@ void LoopPipeliner::emitPrologue() {
               lookupOrDefault(loadOp.getPtr(), stage), newMask,
               lookupOrDefault(loadOp.getOther(), stage),
               loadOp.getBoundaryCheckAttr(), loadOp.getPaddingAttr(),
-              loadOp.getCache(), loadOp.getEvict(), loadOp.getIsVolatile());
+              loadOp.getCache(), loadOp.getEvict(), loadOp.getIsVolatile(),
+              loadOp.getContiguityInterConstGroup());
 #ifdef USE_MACA
-          addNamedAttrs(newOp, op->getAttrDictionary());
+          addNamedAttrs(newOp, op->getDiscardableAttrDictionary());
 #else
           addNamedAttrs(newOp, op->getDiscardableAttrDictionary());
 #endif
@@ -1238,9 +1240,10 @@ void LoopPipeliner::prefetchNextIteration(scf::ForOp newForOp,
             curMapping.lookupOrDefault(loadOp.getPtr()), newMask,
             curMapping.lookupOrDefault(loadOp.getOther()),
             loadOp.getBoundaryCheckAttr(), loadOp.getPaddingAttr(),
-            loadOp.getCache(), loadOp.getEvict(), loadOp.getIsVolatile());
+            loadOp.getCache(), loadOp.getEvict(), loadOp.getIsVolatile(),
+            loadOp.getContiguityInterConstGroup());
 #ifdef USE_MACA
-        addNamedAttrs(nextOp, op->getAttrDictionary());
+        addNamedAttrs(nextOp, op->getDiscardableAttrDictionary());
 #else
         addNamedAttrs(nextOp, op->getDiscardableAttrDictionary());
 #endif
@@ -1281,7 +1284,8 @@ void LoopPipeliner::prefetchNextIteration(scf::ForOp newForOp,
           nextMapping.lookupOrDefault(loadOp.getPtr()), newMask,
           nextMapping.lookupOrDefault(loadOp.getOther()),
           loadOp.getBoundaryCheckAttr(), loadOp.getPaddingAttr(),
-          loadOp.getCache(), loadOp.getEvict(), loadOp.getIsVolatile());
+          loadOp.getCache(), loadOp.getEvict(), loadOp.getIsVolatile(),
+          loadOp.getContiguityInterConstGroup());
       nextLoads.push_back(newLoadOp->getResult(0));
       genLoads.push_back(newLoadOp->getResult(0));
       auto curLoadOp = *nextLoads.begin();
